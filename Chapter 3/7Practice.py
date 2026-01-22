7b4bb2030734252555cd14f9c695a2a6f74ca469
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2026-01-22 09:06:37 +0600

    Completed chapter3 List

--- Chapter 3/7Practice.py
+++ Chapter 3/7Practice.py
@@ -1,1 +1,3 @@
 #WAP to count the number of students with the "A" grade in thr following tuple.
+grade = ("C","D","A","A","B","B","A")
+print(grade.count("A"))
---
+++ Chapter 3/8Practice.py
@@ -0,0 +1,5 @@
+#store the above values in a list & sort them form "A" to "B".
+list = ["C","D","A","A","B","B","A"]
+
+list.sort()
+print(list)
