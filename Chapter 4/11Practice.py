ff74d88c8040f8f52561e448a9385fa106b29d00
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2026-01-24 23:47:17 +0600

    Chapter4 question

--- Chapter 4/11Practice.py
+++ Chapter 4/11Practice.py
@@ -1,2 +1,14 @@
 '''WAP to enter marks of 3 subjects from the user and store them in a dictionary. Start with
 an empty dictionary & add one by one. Use subject name as key & marks as value.'''
+marks = {}
+
+x = int(input("enter phy :"))
+marks.update({"phu":x})
+
+x = int(input("enter math :"))
+marks.update({"math":x})
+
+x = int(input("enter chem :"))
+marks.update({"chem":x})
+
+print(marks)
