c49bbc970f02efa1f7635b80d0615dc633073bfc
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2026-01-29 22:48:28 +0600

    Chapter5 Loop

---
+++ Chapter 5/11Practice.py
@@ -0,0 +1,12 @@
+# Search for a number x in this tuple using loop:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+list = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+x = 36
+idx = 0
+for value in list:
+    if(value==x):
+        print("found at idx", idx)
+    idx+=1
+    # else:
+    #     print("Not found")    
