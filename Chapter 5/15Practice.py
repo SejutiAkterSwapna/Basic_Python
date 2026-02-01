72abefac7bc1fa659147031117ef2053d3637667
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2026-02-01 23:20:46 +0600

    learn complete Loop

---
+++ Chapter 5/15Practice.py
@@ -0,0 +1,18 @@
+##QU-> WAP to find the sum of first n numbers. (using while)
+# n = 5
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+
+# print("Sum is = ",sum)
+
+#_______________
+
+num = 7
+sum = 0
+i = 1
+while i <= num:
+    sum+=i
+    i+=1
+
+print(sum)
