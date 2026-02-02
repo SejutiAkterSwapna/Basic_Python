cb1980884d9d3725bba9f4234548765b1d6f5951
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2026-02-02 23:30:08 +0600

    start new chapter6 , Function

---
+++ Chapter 6/1Function.py
@@ -0,0 +1,30 @@
+def calc_sum(a,b): ## Function definition.
+    sum = a+b
+    print(sum)
+    return(sum)
+    #return(a + b)
+
+calc_sum(3,6) ## (calc_sum)->function call, ## (3,6)->arguments.
+calc_sum(7,4)
+calc_sum(3,9)
+calc_sum(2,6)
+
+def print_Hw():
+    print("Hello world")
+
+print_Hw()
+print_Hw()
+print_Hw()
+print_Hw()
+print_Hw()
+
+##QU-> Average of three numbers
+# def ave_of_3num(a,b,c):
+#     sum = a + b + c
+#     average = sum / 3
+#     print(average)
+#     return(average)
+
+# ave_of_3num(1,2,3)
+# ave_of_3num(4,2,3)
+# ave_of_3num(4,5,3)
