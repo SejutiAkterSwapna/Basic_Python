d983c67704998f3d3a2b03c35875c1cfbc73f468
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2026-02-03 23:11:47 +0600

    Function learning

---
+++ Chapter 6/3Prastice.py
@@ -0,0 +1,9 @@
+##QU->WAF to print the length of a list. ( list is the parameter)
+list1 = ["cat","rat","caw","dog"]
+list2 = ["A","E","I","O","U"]
+def print_len(list):
+    print(len(list))
+    return(list)
+
+print_len(list1)
+print_len(list2)
